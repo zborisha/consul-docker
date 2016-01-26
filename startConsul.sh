60cb812f10c25601f6046c8485810722f1822917
Borisa Zivkovic <borisa.zivkovic@huawei.com>
2016-01-26 16:24:01 +0000

    fixing advertise address

--- startConsul.sh
+++ startConsul.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+if [[ -z $ADVERTISE ]]; then export ADVERTISE="127.0.0.1"; fi
 
-/opt/consul/consul agent -ui -ui-dir=/opt/consul/ui/ -data-dir=/opt/consul/data/ -dev -client=0.0.0.0
+/opt/consul/consul agent -ui -ui-dir=/opt/consul/ui/ -data-dir=/opt/consul/data/ -dev -client=0.0.0.0 -advertise=$ADVERTISE
